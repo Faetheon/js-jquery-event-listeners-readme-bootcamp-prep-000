cec31979ee54f0072ba0998c579fbc07435be0d7
Nathan Hill <nateohill@gmail.com>
2018-05-21 17:45:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,12 @@ function getIt() {
   $('p').click(alert('Hey!'));
 }
 
+function frameIt() {
+  $('.tasty').on('load', function() { 
+    $('.tasty').css('border-color: red');
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
